e99f945b9864110935daebea613c1e525d9c05cb
GabyLudewig <64235704+GabyLudewig@users.noreply.github.com>
2021-11-26 15:16:50 -0600

    js orden de Amigos

---
+++ js/ordenAmigos.js
@@ -0,0 +1,17 @@
+
+let amigos = [];
+let orden = [];
+
+function ordenaAmigos(a, b){
+    let numAmigos = prompt("¿Cuantas amigos vas a ingresar?")
+    
+    for (i = 0; i < numAmigos; i++) {
+        nombres = prompt("¿Cual es el nombre de tu amigo " + (i+1) + "?")
+        amigos.push(nombres)
+              }
+    orden = amigos.sort();          
+    
+    orden.forEach(element => console.log(element));
+        
+};
+ordenaAmigos();

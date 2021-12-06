9596a8098262598d4444094ba730e636ba224f46
GabyLudewig <64235704+GabyLudewig@users.noreply.github.com>
2021-12-06 04:33:05 -0600

    Foto amigo aleatorio

--- js/api.js
+++ js/api.js
@@ -1,16 +1,32 @@
-const url = 'https://randomuser.me/';
-let resultado;
-
-function getUsuariosRandom (id){
-    id= 'https://randomuser.me/api/'
-    fetch(id)
+const app = document.querySelector('.container')
+   const url = 'https://randomuser.me/api/';
+    fetch(url)
     .then (response => response.json())
     .then (json => {
+        
         console.log(json)
         resultado = json
-    })
-    .catch (error => {
-        console.log('No hay respuesta')
-    })
-}
+        console.log(resultado.results[0].picture.large)
+        
+        const img = document.createElement('img')    
+        img.src = resultado.results[0].picture.large
+        app.appendChild(img)
+        
+        const p = document.createElement('p')    
+        p.innerHTML = (resultado.results[0].name.title) + "<br> " +(resultado.results[0].name.first) + " " + (resultado.results[0].name.last)
+        app.appendChild(p)
 
+        const c = document.createElement('c')    
+        c.innerHTML = "Email: " + (resultado.results[0].email) + "<br>" + "<br>"+ "Celular: " + (resultado.results[0].cell)
+        app.appendChild(c)
+
+     })
+        .catch (error => console.log('No hay respuesta'))
+      ;
+            
+    
+    
+    
+    
+    
+    

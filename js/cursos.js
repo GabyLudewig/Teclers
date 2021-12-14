d9548fc93ba59bad8ddb0c306375ef44168b2317
mane1807 <hernandezgutierrezjuan7@gmail.com>
2021-12-14 13:14:37 -0600

    nombres

---
+++ js/cursos.js
@@ -0,0 +1,12 @@
+let cursos= ['java','phyton','Unreal Engine ','c++'];
+
+var cantidad=cursos.length;
+console.log('usted a realizado',cantidad,'cursos')
+cursos.forEach(element => {
+console.log(element)
+     });
+        
+
+
+
+

175210e66f6e289a9a169ed601340432dcc4b150
mane1807 <hernandezgutierrezjuan7@gmail.com>
2021-12-17 20:57:44 -0600

    tablas base de datos

--- db/index.js
+++ db/index.js
@@ -6,10 +6,40 @@ async function findAllRows(){
 
     sequelize.query("select * from usuarios",{type:sequelize.QueryTypes.SELECT})
     .then(function(personas){
+        console.log('Tabla Usuarios')
         console.log(personas);
       })
 
-}
+      sequelize.query("select * from cursos",{type:sequelize.QueryTypes.SELECT})
+    .then(function(personas){
+        console.log('Tabla cursos ')
+        console.log(personas);
+      })
+
+      sequelize.query("select * from amigos",{type:sequelize.QueryTypes.SELECT})
+    .then(function(personas){
+        console.log('Tabla amigos ')
+        console.log(personas);
+      })
+
+      sequelize.query("select * from publicaciones",{type:sequelize.QueryTypes.SELECT})
+    .then(function(personas){
+        console.log('Tabla Publicaciones ')
+        console.log(personas);
+      })
+
+      sequelize.query("select * from perfil",{type:sequelize.QueryTypes.SELECT})
+    .then(function(personas){
+        console.log('Tabla Perfil')
+        console.log(personas);
+      })
+
+
+
+
+
+} 
+
 
 findAllRows();
 

1d276b094579fe19f5204fb2170d37ea8c15485a
GabyLudewig <64235704+GabyLudewig@users.noreply.github.com>
2022-02-10 16:41:28 -0600

    back users

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(cors());
 
 //3-Invocamos a dotenv
 const dotenv = require("dotenv");
-dotenv.config();
+dotenv.config({path:'/ENV'});
 
 //4- el directorio public
 app.use('/resources', express.static('public'));
